f741e0dfca9d92b8776d77a41ae336e2c2a8e787
Facundo Villagra <subofer@hotmail.com>
2021-01-25 14:57:05 -0300

    notificaciones simplificadas

--- js/notificaciones.js
+++ js/notificaciones.js
@@ -1,15 +1,14 @@
 function notificar(item,cantidad) {
   $(".notify").toggleClass("active");
 
-  if(cantidad == 1){
-    cantidad = `agrego ${cantidad}Kg`
-  }else{
-    cantidad = `agregaron ${cantidad}Kg`
-  }
-  
-  $(".success").attr('data-before',`Se ${cantidad} de ${mayuscula(item.nombre)} al carrito`);
+  let palabra = "agrego"
+    if(cantidad>1){palabra = "agregaron"}
+
+  $(".success").attr('data-before',`Se ${palabra} ${cantidad}Kg de ${mayuscula(item.nombre)} al carrito`);
 
   setTimeout(function(){
+    $(".success").attr('data-before',``);
     $(".notify").removeClass("active");
-  },2000);
+    },2000);
 };
+
