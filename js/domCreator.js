d02e53231e09df3a653ef6380be3be4e2a04cd1a
Facundo Villagra <subofer@hotmail.com>
2021-01-25 15:01:29 -0300

    notificaciones simplificadas

--- js/domCreator.js
+++ js/domCreator.js
@@ -50,4 +50,21 @@ document.addEventListener('cambios_en_carro', e => {
 	
 }, false);
 
+
+function notificar(item,cantidad) {
+  $(".notify").toggleClass("active");
+  let palabra = "agrego"
+    if(cantidad>1){palabra = "agregaron"}
+  $(".success").attr('data-before',`Se ${palabra} ${cantidad}Kg de ${mayuscula(item.nombre)} al carrito`);
+  setTimeout(function(){
+    $(".success").attr('data-before',``);
+    $(".notify").removeClass("active");
+    },2000);
+};
+
+
+
+
+
+
 updateAll()
